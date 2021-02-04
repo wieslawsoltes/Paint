9be67e1f3828828237e8a158315865a14ef43333
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-02-04 15:57:47 +0100

    Added ImageViewModel stub

---
+++ Paint/ViewModels/ImageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ReactiveUI;
+
+namespace Paint.ViewModels
+{
+    public class ImageViewModel : ViewModelBase
+    {
+        private double _width;
+        private double _height;
+
+        public double Width
+        {
+            get => _width;
+            set => this.RaiseAndSetIfChanged(ref _width, value);
+        }
+
+        public double Height
+        {
+            get => _height;
+            set => this.RaiseAndSetIfChanged(ref _height, value);
+        }
+    }
+}
