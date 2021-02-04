1b2403de88ced9df37dde04b18a568676ee6b814
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-02-04 18:28:31 +0100

    Create ImagePanel.cs

---
+++ Paint/Controls/ImagePanel.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Paint.ViewModels;
+using SixLabors.ImageSharp;
+
+namespace Paint.Controls
+{
+    public class ImagePanel : Panel
+    {
+        public override void Render(DrawingContext context)
+        {
+            base.Render(context);
+
+            if (DataContext is ImageViewModel vm)
+            {
+                if (vm.Bitmap is not null)
+                {
+                    context.DrawImage(vm.Bitmap, vm.Bounds);
+                }
+            }
+        }
+    }
+}
