0e19b65679d526cd170c16885ce37fe57c5bedf8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-02-04 14:23:49 +0100

    Initial import

---
+++ Paint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.ReactiveUI;
+
+namespace Paint
+{
+    class Program
+    {
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace()
+                .UseReactiveUI();
+    }
+}
